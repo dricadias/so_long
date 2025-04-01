5d480ad5a1796e55c194befd65ed553b191ff38a
dricadias <89052954+dricadias@users.noreply.github.com>
2025-04-01 18:57:31 +0100

    printing xmp on window

--- srcs/rendering.c
+++ srcs/rendering.c
@@ -12,11 +12,6 @@
 
 #include "../include/so_long.h"
 
-// renderizar o mapa na tela
-// colocar as imagens em cada posição (chão, parede, jogador, saída, etc.).
-// desenhar apenas blocos coloridos por agora.
-
-/*
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -43,57 +38,21 @@ void	put_square(t_data *img, int x, int y, int size, int color)
 	}
 }
 
-void rendering_map(char *file)
+void rendering_map(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	void		*mlx;
+	void		*mlx_win;
 	t_data	img;
-	t_long	pos_y;
-	t_long	pos_x;
+	char		*path = "./quadrado_cinza.xpm";
+	void		*img_quadrado;
+	int			img_width;
+	int			img_height;
 
-	pos_x.x = 0;
-	pos_y.y = 0;
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1280, 720, "so_long");
-
-	img.img = mlx_new_image(mlx, 1280, 720);
+	mlx_win = mlx_new_window(mlx, 720, 480, "so_long");
+	img.img = mlx_new_image(mlx, 720, 480);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while(*file)
-	{
-		put_square(&img, pos_x.x, pos_y.y, 75, 0x00999999);
-		mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-		pos_x.x += 75;
-		//pos_y.y += 100;
-		file++;
-	}
+	img_quadrado = mlx_xpm_file_to_image(mlx, path, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, img_quadrado, 0, 0);
 	mlx_loop(mlx);
 }
-
-void rendering_map(char *file)
-{
-	void	*mlx;
-	void	*mlx_win;
-	void	*img;
-	char	*path = "../map.ber";
-	int		img_width;
-	int		img_height;
-	t_long	pos_y;
-	t_long	pos_x;
-
-	pos_x.x = 0;
-	pos_y.y = 0;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1280, 720, "so_long");
-
-	//img = mlx_xpm_to_image(mlx, path,	&img_width, &img_height);
-	//img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while(*file)
-	{
-		//put_square(&img, pos_x.x, pos_y.y, 75, 0x00999999);
-		mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
-		pos_x.x += 75;
-		//pos_y.y += 100;
-		file++;
-	}
-	mlx_loop(mlx);
-}*/
