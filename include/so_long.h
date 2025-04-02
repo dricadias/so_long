4a9639639bdf1df2f97beaf5f3be8b1262d80bb3
dricadias <andrielly.12dias.ad@gmail.com>
2025-04-02 02:55:52 +0100

    add struct and functions to put image

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 12:08:53 by adias-do          #+#    #+#             */
-/*   Updated: 2025/03/31 20:36:15 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/04/02 02:53:16 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <stdio.h>
 # include <errno.h>
 # include <fcntl.h>
+# define TILE_SIZE 50
 
 typedef struct s_pos
 {
@@ -43,18 +44,29 @@ typedef struct s_data
 	int		endian;
 }	t_data;
 
+typedef struct s_game {
+    void    *mlx;
+    void    *mlx_win;
+    void    *img_wall;
+    void    *img_floor;
+    void    *img_player;
+    void    *img_exit;
+    t_map   *map;
+}   t_game;
+
 // functions
 int		valid_file(char *file);
 
 // matriz
 void	fill_map(t_map *map, char *file);
-char	**create_map(char *file);
+char	**create_map(t_map *map, char *file);
 int		get_width(char *file);
 int		get_height(char *file);
 
 // mlx
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
 void	put_square(t_data *img, int x, int y, int size, int color);
-void	rendering_map(void);
+void	rendering_map(t_game *game);
+void	load_img(t_game *game);
 
 #endif
