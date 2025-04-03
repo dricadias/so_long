13abd214dbe23ea1597bfa38c4960c57b6f32932
dricadias <andrielly.12dias.ad@gmail.com>
2025-04-03 23:40:41 +0100

    organizing better

--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,51 +6,51 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 14:41:56 by adias-do          #+#    #+#             */
-/*   Updated: 2025/04/02 02:26:57 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/04/03 23:36:01 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+void	print_map(t_map map)
+{
+	int		x;
+	int		y;
+
+	x = 0;
+	while (x < map.height && map.matriz[x])
+	{
+		y = 0;
+		while (y < map.width && map.matriz[x][y])
+		{
+			ft_printf("%c", map.matriz[x][y]);
+			y++;
+		}
+		ft_printf("\n");
+		x++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
+	int		ret;
+	t_map	map;
+
 	if (argc == 2)
 	{
-		int	ret;
-
 		ret = valid_file(argv[1]);
 		if (ret == 1)
 		{
-			t_map	map;
-			int		x;
-			int		y;
-			int		height = get_height(argv[1]);
-			int		width = get_width(argv[1]);
-
-			map.matriz = create_map(&map, argv[1]);
-			fill_map(&map, argv[1]);
-
-			x = 0;
-			while(x < height)
-			{
-				y = 0;
-				while(y < width)
-				{
-					ft_printf("%c", map.matriz[x][y]);
-					y++;
-				}
-				ft_printf("\n");
-				x++;
-			}
-			/*t_game game;
+			//print_map(map);
+			t_game game;
+			
 			game.mlx = mlx_init();
 			game.map = malloc(sizeof(t_map));
-			game.map->matriz = create_map(game.map, argv[1]);
-			fill_map(game.map, argv[1]);
+			set_values(game.map, argv[1]);
 			game.mlx_win = mlx_new_window(game.mlx, game.map->width * TILE_SIZE, game.map->height * TILE_SIZE, "so_long");
 			load_img(&game);
 			rendering_map(&game);
-			mlx_loop(game.mlx);*/
+			mlx_loop(game.mlx);
 		}
 		else
 		{
@@ -58,26 +58,6 @@ int	main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 		}
 	}
+	free_map(&map);
 	return (0);
 }
-
-			/*t_map	map;
-			int		x;
-			int		y;
-			int		height = get_height(argv[1]);
-			int		width = get_width(argv[1]);
-
-			map.matriz = create_map(&map, argv[1]);
-			fill_map(&map, argv[1]);
-
-			x = 0;
-			while(x < height)
-			{
-				y = 0;
-				while(y < width)
-				{
-					ft_printf("%c", map.matriz[x][y]);
-					y++;
-				}
-				x++;
-			} */
