f70559ccb933d1668f98f6dbd47992805dfd7da3
dricadias <andrielly.12dias.ad@gmail.com>
2025-04-05 22:46:01 +0100

    fixing norm errors (not 100%)

--- srcs/map.c
+++ srcs/map.c
@@ -6,7 +6,7 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 12:02:02 by adias-do          #+#    #+#             */
-/*   Updated: 2025/04/05 20:33:27 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/04/05 22:44:10 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,9 @@ void	is_valid_characters(char *content)
 	int	i;
 
 	i = 0;
-
 	while (content[i])
 	{
-		if (content[i] != '0' && content[i] != '1' && content[i] != 'P' 
+		if (content[i] != '0' && content[i] != '1' && content[i] != 'P'
 			&& content[i] != 'E' && content[i] != 'C' && content[i] != '\n')
 		{
 			return ((void)ft_printf("map isnt valid\n"), exit(EXIT_FAILURE));
@@ -40,7 +39,7 @@ void	is_map_rectangular(char **matriz)
 
 	i = 0;
 	len = ft_strlen(matriz[0]);
-	while(matriz[i])
+	while (matriz[i])
 	{
 		len2 = ft_strlen(matriz[i]);
 		if (len != len2)
