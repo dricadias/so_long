8d91318ea0fd34b598b080057222aeee142fde10
dricadias <andrielly.12dias.ad@gmail.com>
2025-04-09 23:59:07 +0100

    functions to clear and exit the game safely

--- srcs/free.c
+++ srcs/free.c
@@ -42,3 +42,47 @@ void	ft_exit(char *error_msg, t_map *map, int status)
 	}
 	exit(status);
 }
+
+void	free_images(t_game *game)
+{	
+	if (game->wall)
+		mlx_destroy_image(game->mlx, game->wall);
+	if (game->floor)
+		mlx_destroy_image(game->mlx, game->floor);
+	if (game->player)
+		mlx_destroy_image(game->mlx, game->player);
+	if (game->coll)
+		mlx_destroy_image(game->mlx, game->coll);
+	if (game->exit)
+		mlx_destroy_image(game->mlx, game->exit);
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+}
+
+void	free_all(t_game *game)
+{
+	if (!game)
+		return ;
+	if (game->map)
+	{
+		if (game->map->matriz)
+			free_matriz(game->map->matriz);
+		if (game->map->flood)
+			free_matriz(game->map->flood);
+		free(game->map);
+	}
+	free_images(game);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
+	free(game);
+}
+
+void	exit_game(char *msg, t_game *game, int status)
+{
+	ft_putendl_fd(msg, 2);
+	free_all(game);
+	exit(status);
+}
