7075dced4cffaf941663c34a48d4774ccb6bbe3f
dricadias <89052954+dricadias@users.noreply.github.com>
2025-04-10 17:18:46 +0100

    game textures

--- srcs/rendering.c
+++ srcs/rendering.c
@@ -6,7 +6,7 @@
 /*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 14:27:04 by adias-do          #+#    #+#             */
-/*   Updated: 2025/04/09 22:38:57 by adias-do         ###   ########.fr       */
+/*   Updated: 2025/04/10 15:54:51 by adias-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	load_img(t_game *game)
 	int	width;
 	int	height;
 
-	game->wall = mlx_xpm_file_to_image(game->mlx, "textures/quadrado_cinza50.xpm", &width, &height);
-	game->floor = mlx_xpm_file_to_image(game->mlx, "textures/quadrado_verde.xpm", &width, &height);
-	game->coll = mlx_xpm_file_to_image(game->mlx, "textures/quadrado_amarelo.xpm", &width, &height);
-	game->player = mlx_xpm_file_to_image(game->mlx, "textures/quadrado_azul.xpm", &width, &height);
-	game->exit = mlx_xpm_file_to_image(game->mlx, "textures/quadrado_vermelho.xpm", &width, &height);
+	game->wall = mlx_xpm_file_to_image(game->mlx, "textures/wall.xpm", &width, &height);
+	game->floor = mlx_xpm_file_to_image(game->mlx, "textures/floor.xpm", &width, &height);
+	game->coll = mlx_xpm_file_to_image(game->mlx, "textures/coll.xpm", &width, &height);
+	game->player = mlx_xpm_file_to_image(game->mlx, "textures/player.xpm", &width, &height);
+	game->exit = mlx_xpm_file_to_image(game->mlx, "textures/exit.xpm", &width, &height);
 }
 
 void	render_map(t_game *game)
